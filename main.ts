4f80f57ae3f6bdeac0397001ad2a60bb3b295ca3
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-06-16 15:20:59 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 radio.onReceivedNumber(function (receivedNumber) {
     t0 = Math.round(input.runningTime() * 1000) / 1000
-    while (input.lightLevel() > 180 && t1 == 0) {
+    while (input.lightLevel() > 20 && t1 == 0) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -13,10 +13,15 @@ radio.onReceivedNumber(function (receivedNumber) {
     basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.A, function () {
+    radio.setGroup(20)
     Temps = (t1 - t0) / 1000
-    for (let index = 0; index < 4; index++) {
+    radio.sendValue("Ligne 2", Temps)
+    for (let index = 0; index < 2; index++) {
         basic.showString("" + Temps + "s")
     }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (input.lightLevel()))
 })
 let Temps = 0
 let t1 = 0
