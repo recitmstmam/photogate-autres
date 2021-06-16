0598119c7c559d17799b7afcbe766e3c67a2bc3d
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-06-16 11:50:17 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    t0 = Math.round(input.runningTime() * 1000) / 1000
+    while (input.lightLevel() > 180 && t1 == 0) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+    t1 = Math.round(input.runningTime() * 1000) / 1000
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.A, function () {
+    Temps = (t1 - t0) / 1000
+    for (let index = 0; index < 4; index++) {
+        basic.showString("" + Temps + "s")
+    }
+})
+let Temps = 0
+let t1 = 0
+let t0 = 0
+radio.setGroup(1)
+t0 = 0
+t1 = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+pins.digitalWritePin(DigitalPin.P0, 1)
